f661991bca75e52e809a37514bb7fc816d502e21
Jagandeep Singh <133378199+Jagandeep-Singh@users.noreply.github.com>
2024-11-07 21:52:42 +0530

    Analysis.sql

--- Analysis.sql
+++ Analysis.sql
@@ -1,8 +1,19 @@
-Marketing Analysis
-1 - Rewarding Most Loyal Users
+-- Marketing Analysis
+-- Rewarding Most Loyal Users
 
 SYNTAX:
 select * from users;
 select username, created_at from  users
 order by created_at limit 5;
 
+-- Remind Inactive Users to Start Posting
+
+SYNTAX:
+select * from users, photos;  select * from users
+left join photos	on  users.id=photos.user_id
+where photos.image_url is null
+order by users.username
+
+3 - Declaring Contest Winner
+SYNTAX:
+select * from likes, photos, users;
